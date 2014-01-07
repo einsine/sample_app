5120ce42bba1cc64b4cf0054d708ca43263236c8
Sine <einsine@gmail.com>
2014-01-07 11:33:51 +0000

    add user following

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,16 +1,28 @@
 class User < ActiveRecord::Base
 	before_save {self.email = email.downcase}
   has_many :microposts, dependent: :destroy
+  has_many :relationships, foreign_key: "follower_id", dependent: :destroy
+  has_many :followed_users, through: :relationships, source: :followed
+
+  has_many :reverse_relationships, foreign_key: "followed_id",
+                                   class_name:  "Relationship",
+                                   dependent:   :destroy
+  has_many :followers, through: :reverse_relationships, source: :follower
 	validates :name, presence: true, length: {maximum: 50}
 	VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
-    validates :email, presence: true, format: { with: VALID_EMAIL_REGEX }, uniqueness: {case_sensitive: false}
-  	has_secure_password
-  	validates :password, length: { minimum: 6 }
-  	before_create :create_remember_token
+  validates :email, presence: true, format: { with: VALID_EMAIL_REGEX }, uniqueness: {case_sensitive: false}
+  has_secure_password
+  validates :password, length: { minimum: 6 }
+  before_create :create_remember_token
 
+    
+    
     def feed
-      Micropost.where("user_id = ?",id)
+      Micropost.from_users_followed_by(self)
     end
+    
+
+    
 
   	
   	def User.new_remember_token
@@ -20,6 +32,17 @@ class User < ActiveRecord::Base
   	def User.encrypt(token)
   		Digest::SHA1.hexdigest(token.to_s)
   	end
+
+    def following?(other_user)
+      relationships.find_by(followed_id: other_user.id)
+    end
+
+    def follow!(other_user)
+      relationships.create!(followed_id: other_user.id)
+    end
+    def unfollow!(other_user)
+    relationships.find_by(followed_id: other_user.id).destroy!
+  end
 
   	private
   		def create_remember_token
